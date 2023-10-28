88bd6755c0fff385f14199e9ae9c2959e10a496d
Archie <archie_alquiros@dlsu.edu.ph>
2023-10-28 19:43:49 +0800

    add link to each buttons in search related pages

--- app/src/main/java/ph/edu/dlsu/mobdeve/s15/alquiros/archie/projectdraft1/Exercise.java
+++ app/src/main/java/ph/edu/dlsu/mobdeve/s15/alquiros/archie/projectdraft1/Exercise.java
@@ -3,12 +3,20 @@ package ph.edu.dlsu.mobdeve.s15.alquiros.archie.projectdraft1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Exercise extends AppCompatActivity {
+
+    private Button AddWorkout1;
+    private Button AddWorkout2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
+
+        AddWorkout1=(Button) findViewById(R.id.AddWorkButton1);
+        AddWorkout2=(Button) findViewById(R.id.AddWorkoutButton2);
     }
 }
--- app/src/main/java/ph/edu/dlsu/mobdeve/s15/alquiros/archie/projectdraft1/SearchItem.java
+++ app/src/main/java/ph/edu/dlsu/mobdeve/s15/alquiros/archie/projectdraft1/SearchItem.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SearchItem extends AppCompatActivity {
@@ -15,16 +17,42 @@ public class SearchItem extends AppCompatActivity {
 
     TextView name;
 
+    private Button FoodButton1;
+    private Button FoodButton2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_item);
 
+        FoodButton1=(Button) findViewById(R.id.AddButtonFood1);
+        FoodButton2=(Button) findViewById(R.id.AddButtonFood2);
+
+        FoodButton1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                BackToMain();
+            }
+        });
+
+        FoodButton2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                BackToMain();
+            }
+        });
+
         Intent intent = getIntent();
         searchtype=intent.getStringExtra("type");
 
-
-
     }
+
+    public void BackToMain()
+    {
+        Intent intent = new Intent (this, MainPage.class);
+        startActivity(intent);
+    }
+
 }
