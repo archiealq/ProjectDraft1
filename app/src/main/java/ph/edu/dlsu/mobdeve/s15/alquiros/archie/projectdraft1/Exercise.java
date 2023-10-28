0c19e951b36ddb36f5d357455dfc3230287a1191
Archie <archie_alquiros@dlsu.edu.ph>
2023-10-28 19:48:09 +0800

    add links to buttons to return to main

--- app/src/main/java/ph/edu/dlsu/mobdeve/s15/alquiros/archie/projectdraft1/Exercise.java
+++ app/src/main/java/ph/edu/dlsu/mobdeve/s15/alquiros/archie/projectdraft1/Exercise.java
@@ -2,7 +2,9 @@ package ph.edu.dlsu.mobdeve.s15.alquiros.archie.projectdraft1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Exercise extends AppCompatActivity {
@@ -18,5 +20,25 @@ public class Exercise extends AppCompatActivity {
 
         AddWorkout1=(Button) findViewById(R.id.AddWorkButton1);
         AddWorkout2=(Button) findViewById(R.id.AddWorkoutButton2);
+
+        AddWorkout1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                AddToMain();
+            }
+        });
+
+        AddWorkout2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                AddToMain();
+            }
+        });
+    }
+
+    public void AddToMain()
+    {
+        Intent intent = new Intent(this, MainPage.class);
+        startActivity(intent);
     }
 }
